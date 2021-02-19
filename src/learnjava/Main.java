d696a9713ac687b5b5008ecb60d1ce1e4260f8ca
Joshua O'Bannon <jtobannon@gmail.com>
2021-02-18 23:15:20 -0700

    working

---
+++ src/learnjava/Main.java
@@ -0,0 +1,13 @@
+package learnjava;
+
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("super enter something");
+        System.out.println("super"");
+    }
+}
